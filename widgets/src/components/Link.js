2a410b71ef41f4a51660e229fe69715ab726462e
Andreia Cifoni <andreiacifoni@gmail.com>
2022-04-20 15:26:14 -0400

    EventListener

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     window.history.pushState({}, "", href);
+    //the purpose of this next code is to tell the components that the URL just changed
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
   };
 
   return (
